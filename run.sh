824df2bfebd28d218cf8e3f22101b4ea2efd5223
kenny <kenny.ma01@sap.com>
2022-05-26 09:03:47 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ group_name='mk100120'
 # 定义应用名称
 app_name='hello-sunday'
 # 定义应用版本
-app_version='1.0.0'
+app_version='2.0.0'
 # 定义应用环境
 #profile_active='qa'
 echo '----copy jar----'
@@ -17,11 +17,14 @@ echo '----rm image----'
 # 打包编译docker镜像
 echo '----build image----'
 docker build -t ${group_name}/${app_name}:${app_version} .
-echo '----start container----'
-docker run -p 8088:8088 --name ${app_name}  -d ${group_name}/${app_name}:${app_version}
 
-echo '-------test jar -----------'
-curl -v localhost:8088/biz/v1/sunday/app
+docker image ls |grep ${app_name}
 
-echo '----stop container----'
-docker container stop ${app_name}
+#echo '----start container----'
+#docker run -p 8088:8088 --name ${app_name}  -d ${group_name}/${app_name}:${app_version}
+
+#echo '-------test jar -----------'
+#curl -v localhost:8088/biz/v1/sunday/app
+
+#echo '----stop container----'
+#docker container stop ${app_name}
